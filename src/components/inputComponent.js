fbadec207aa939398d52e9992d7bbc7b833e648d
FVukmirovic <fvukmirovic@gmail.com>
2020-11-29 20:42:56 +0100

    small improvement
    Removed a dot from placeholder

--- src/components/inputComponent.js
+++ src/components/inputComponent.js
@@ -6,7 +6,7 @@ export default class InputComponent extends React.Component {
       <div className="inputdiv">
         <input id={this.props.name} name={this.props.name} placeholder={this.props.text} type={this.props.type} value={this.props.value} onChange={this.props.change} onBlur={this.props.blur}/>
         <p  className="error" >  
-        <span className="holder">.</span>
+        {/* <span className="holder">.</span> */}
           {this.props.error.length > 0 &&  <span>{this.props.error}</span>}
         </p>
       </div>
