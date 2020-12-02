8d878340aaff49bd8f46887d09ab41edae3f69f5
milica-vojinovic <72526275+milica-vojinovic@users.noreply.github.com>
2020-12-02 23:38:31 +0100

    inputs added

---
+++ src/components/DatePickerComponent.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import DatePicker from "react-date-picker";
+ 
+import "react-date-picker/dist/DatePicker.css";
+
+export default class DatePickerComponent extends React.Component {
+
+  render() { 
+    return (
+      <div className="cvd-inputdiv">
+        <label className="cvd-h2">{this.props.label}</label>
+        <div>
+          <DatePicker  name={this.props.name} value={this.props.value} selected={this.props.selected} onSelect={this.props.change} onChange={this.props.change} onBlur={this.props.blur} dateFormat="dd/MM/yyyy" showYearDropdown scrollableMonthYearDropdown/>
+        </div>
+        <p  className="error" >  <span className="holder">.</span>
+          {this.props.error.length > 0 &&  <span>{this.props.error}</span>}
+        </p>
+      </div>
+      
+    );
+  }
+}
+
--- src/personalCVdata.screen.js
+++ src/personalCVdata.screen.js
@@ -1,15 +1,112 @@
 import React from "react";
 import {Redirect, Link} from "react-router-dom";
 import './style/personalCVdata.css';
+import DatePickerComponent from "./components/DatePickerComponent";
+import CVdataSubmit from "./components/CVdataSubmit";
+import CVdataInput from "./components/CVdataInput";
 
 export default class CVdataScreen extends React.Component {
     constructor(props) {
-        super(props);
+        super(props);  
+        this.state = {
+          Mobile: "",
+          Address: "",
+          City: "",
+          Country: "",  
+          DOB: "", 
+          JobPosition: "", 
+          Company: "",
+          StartingDate: "", 
+          Description: "",  
+          Facebook: "", 
+          Instagram: "", 
+          LinkedIn: "", 
+          errors : {
+            Mobile: "",
+            Address: "",
+            City: "",
+            Country: "",  
+            DOB: "", 
+            JobPosition: "", 
+            Company: "",
+            StartingDate: "", 
+            Description: "",  
+            Facebook: "", 
+            Instagram: "", 
+            LinkedIn: "", 
+          } 
+        }
+      }
+      
+      handleChange = e => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        this.setState({[name]: value});
+      }
+    
+      handleDateChange(value) {
+        this.setState({DOB: value});
+      }
 
-        this.state = {};
-    }
+      handleStartDateChange(value) {
+        this.setState({StartingDate: value});
+      }
+
+      handleBlur = (e) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        this.setState({[name]: value});
+        let errors = this.state.errors;
+        switch (name) {
+        //   case 'fullName':
+        //     errors.fullName = value.length < 1 ? 'Full name must be filled in': '';
+        //     break;
+        //   case 'password':
+        //     errors.password = value.length < 1 ? 'Password must be filled in': '';
+        //     break;
+        //   case 'repPassword':
+        //     errors.repPassword = value !== this.state.password ? 'Passwords do not match ': '';
+        //     break;
+          default:
+            errors.[name] = value.length < 1 ? 'Field must be filled in': '';
+            break;
+        }
+        this.setState(Object.assign(this.state, { errors,[name]: value }));
+      }
+    
+      handleSubmit = (e) => {
+        e.preventDefault(); 
+    
+        let validity = true;
+        Object.values(this.state).forEach(
+          (val) => val.length < 1 && (validity = false)
+        );
+        Object.values(this.state.errors).forEach(
+          (val) => val.length > 0 && (validity = false)
+        );
+        if (this.state.password !== this.state.repPassword) {
+          this.setState({
+              password: '',
+              repPassword: ''
+          });
+          alert("Passwords don't match");
+        }
+        else if(validity === true){
+           console.log(`
+           --Submitting--
+           Name: ${this.state.fullName}
+           Email:${this.state.email}
+           Password:${this.state.password}
+           PasswordR:${this.state.repPassword}
+           `);
+        }else{
+           alert("Please fill inputs")
+        }
+      }  
 
     render() {
+        const {errors} = this.state 
+
         return (
 
             <div className="cvd-wrapper">
@@ -19,36 +116,146 @@ export default class CVdataScreen extends React.Component {
                 <div className="cvd-content">
                     <div className="cvd-section cvd-contact">
                         <h1 className="cvd-h1">Contact Info</h1>
-                        <h2 className="cvd-h2">Mobile</h2>
+                        <form onSubmit={this.handleSubmit} noValidate>
+                            <CVdataInput
+                                label="Mobile"
+                                name="Mobile"
+                                text="Mobile"
+                                type="tel"
+                                value={this.state.Mobile}
+                                change={this.handleChange}
+                                blur={this.handleBlur}
+                                error={errors.Mobile}
+                            />
 
-                        <h2 className="cvd-h2">Address</h2>
+                            <CVdataInput
+                                label="Address"
+                                name="Address"
+                                text="Street and Number"
+                                type="text"
+                                value={this.state.Address}
+                                change={this.handleChange}
+                                blur={this.handleBlur}
+                                error={errors.Address}
+                            />
 
-                        <h2 className="cvd-h2">City</h2>
+                            <CVdataInput
+                                label="City"
+                                name="City"
+                                text="City"
+                                type="text"
+                                value={this.state.City}
+                                change={this.handleChange}
+                                blur={this.handleBlur}
+                                error={errors.City}
+                            />
 
-                        <h2 className="cvd-h2">Country</h2>
-
-                        <h2 className="cvd-h2">Date of Birth</h2>
+                            <CVdataInput
+                                label="Country"
+                                name="Country"
+                                text="Country"
+                                type="text"
+                                value={this.state.Country}
+                                change={this.handleChange}
+                                blur={this.handleBlur}
+                                error={errors.Country}
+                            />              
+                            
+                            <DatePickerComponent 
+                                label="Date of Birth"
+                                name="DOB"
+                                value={this.state.DOB}
+                                change={(event) => this.handleDateChange(event)}
+                                selected={this.state.DOB}
+                                error={errors.DOB}
+                            />
+                        </form>
                     </div>
+
                     <div className="cvd-section cvd-work">
                         <h1 className="cvd-h1">Current Position</h1>
-                        <h2 className="cvd-h2">Job Title</h2>
+                        <form onSubmit={this.handleSubmit} noValidate>
+                            <CVdataInput
+                                label="Job Title"
+                                name="JobPosition"
+                                text="Current Job Title"
+                                type="text"
+                                value={this.state.JobPosition}
+                                change={this.handleChange}
+                                blur={this.handleBlur}
+                                error={errors.JobPosition}
+                            /> 
 
-                        <h2 className="cvd-h2">Company</h2>
+                            <CVdataInput
+                                label="Company"
+                                name="Company"
+                                text="Company"
+                                type="text"
+                                value={this.state.Company}
+                                change={this.handleChange}
+                                blur={this.handleBlur}
+                                error={errors.Company}
+                            /> 
 
-                        <h2 className="cvd-h2">Starting Date</h2>
+                            <DatePickerComponent 
+                                label="Starting Date"
+                                name="StartingDate"
+                                value={this.state.StartingDate}
+                                change={(event) => this.handleStartDateChange(event)}
+                                selected={this.state.StartingDate}
+                                error={errors.StartingDate}
+                            />
 
-                        <h2 className="cvd-h2">Description</h2>
+                            <CVdataInput
+                                label="Description"
+                                name="Description"
+                                text="Description"
+                                type="text"
+                                value={this.state.Description}
+                                change={this.handleChange}
+                                blur={this.handleBlur}
+                                error={errors.Description}
+                            />
+                        </form>
                     </div> 
+
                     <div className="cvd-section cvd-social">
                         <h1 className="cvd-h1">Social Networks</h1>
-                        <h2 className="cvd-h2">Facebook</h2>
+                        <form onSubmit={this.handleSubmit} noValidate>
+                            <CVdataInput
+                                label="Facebook"
+                                name="Facebook"
+                                text="Facebook Url"
+                                type="url"
+                                value={this.state.Facebook}
+                                change={this.handleChange}
+                                blur={this.handleBlur}
+                                error={errors.Facebook}
+                            />
 
-                        <h2 className="cvd-h2">Instagram</h2>
+                            <CVdataInput
+                                label="Instagram"
+                                name="Instagram"
+                                text="Instagram Url"
+                                type="url"
+                                value={this.state.Instagram}
+                                change={this.handleChange}
+                                blur={this.handleBlur}
+                                error={errors.Instagram}
+                            />
 
-                        <h2 className="cvd-h2">LinkedIn</h2>
-
-
-                        
+                            <CVdataInput
+                                label="LinkedIn"
+                                name="LinkedIn"
+                                text="LinkendIn Url"
+                                type="url"
+                                value={this.state.LinkedIn}
+                                change={this.handleChange}
+                                blur={this.handleBlur}
+                                error={errors.LinkedIn}
+                            />
+                            
+                        </form>
                     </div>  
                 </div>
                 
