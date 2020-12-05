753787ed415783793cb404d37fc3381c0673cac3
Filip Andjelic <filip.andjelic.private@gmail.com>
2020-12-05 13:39:20 +0100

    New backend URL

--- src/services/constants.service.js
+++ src/services/constants.service.js
@@ -4,5 +4,6 @@
  *  `ConstantsService` handles important variables used globally across the Application.
  */
 export const ConstantsService = {
-    URL_BASE: 'https://balkanadventure.me/booking/api/',
+    URL_BASE: 'https://dev.oykos.me/c2_cv_builder_api/',
+    LOCAL_STORAGE_KEY: 'cv-template-builder-c2'
 };
